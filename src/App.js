a9dbe9c0183358e104e6acae21d7a53610d3ad50
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2022-07-17 12:47:39 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Card from "./components/Card";
 
 const App = () => {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
 };
 
 export default App;
