0b34aa9ea10e1ec84629083c51aa40029993ae02
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2022-08-08 18:32:39 +0200

    updated Filter component

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { useState } from "react";
+import "./Filter.css";
 
-const Filter = ({ jobInfo }) => {
-  const [search, setSearch] = useState("");
-
-  return <div>
-    <input 
-    name="search"
-    type="text"
-    placeholder="Type Language"
-    onChange={e => setSearch(e.target.value)}
-    value={search} />
-  </div>;
+const Filter = ({ filter }) => {
+  return (
+    <div className="Languages filterContainer">
+      <ul>
+        {filter.map((item, index) => (
+          <li className="filterList" key={index}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Filter;
