c52fb10ef417f921f8dbe41910752a873e06e2f5
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2022-08-08 19:08:20 +0200

    added styles

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "./Filter.css";
+import removeIcon from "../icons/icon-remove.svg";
 
 const Filter = ({ filter }) => {
   return (
     <div className="Languages filterContainer">
       <ul>
         {filter.map((item, index) => (
-          <li className="filterList" key={index}>
-            {item}
-          </li>
+          <div className="filterBox">
+            <li className="filterList" key={index}>
+              {item}
+            </li>
+            <div className="iconBox">
+              <img className="removeIcon" src={removeIcon} alt="close" />
+            </div>
+          </div>
         ))}
       </ul>
     </div>
