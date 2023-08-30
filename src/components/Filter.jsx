759397db0febe9c98a790001129280ddd3fa71b9
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2023-08-30 12:20:03 +0200

    add message if any filter was applied

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -4,30 +4,40 @@ import removeIcon from "../icons/icon-remove.svg";
 
 const Filter = ({ filter, removeFilter, clearAllFilter }) => {
   return (
-    <div className={`Languages ${filter.length > 0 && "filterContainer"}`}>
-      <ul className="ulList">
-        {filter.map((item, index) => (
-          <div className="filterBox" key={index}>
-            <li className="filterList">{item}</li>
-            <div className="iconBox">
-              <img
-                onClick={removeFilter}
-                className="removeIcon"
-                src={removeIcon}
-                id={item}
-                alt="close"
-              />
-            </div>
-          </div>
-        ))}
-      </ul>
-      <span
-        onClick={clearAllFilter}
-        className={`clearBtnHide ${filter.length > 0 && "clearBtnShow"}`}
-      >
-        clear
-      </span>
-    </div>
+    <>
+      {
+        filter.length === 0 ? (
+          <h2 className="noFilterMsg">
+            Any filter was applied, select any language to apply the filter.
+          </h2>
+        ) : (
+        <div className={`Languages ${filter.length > 0 && "filterContainer"}`}>
+          <ul className="ulList">
+            {filter.map((item, index) => (
+              <div className="filterBox" key={index}>
+                <li className="filterList">{item}</li>
+                <div className="iconBox">
+                  <img
+                    onClick={removeFilter}
+                    className="removeIcon"
+                    src={removeIcon}
+                    id={item}
+                    alt="close"
+                  />
+                </div>
+              </div>
+            ))}
+          </ul>
+          <span
+            onClick={clearAllFilter}
+            className={`clearBtnHide ${filter.length > 0 && "clearBtnShow"}`}
+          >
+            clear
+          </span>
+        </div>
+        )
+      }
+    </>
   );
 };
 
